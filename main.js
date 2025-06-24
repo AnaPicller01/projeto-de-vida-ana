deade16cbb0c2f6339284396aaebf1f8a91b7205
AnaPicller01 <164193234+AnaPicller01@users.noreply.github.com>
2025-06-24 08:16:08 -0300

    24/06

--- main.js
+++ main.js
@@ -25,12 +25,12 @@ function calculaTempo(tempoObjetivo){
     let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
-    let dias = Math.floor(hpras/24);
+    let dias = Math.floor(horas/24);
 
     segundos %= 60;
     minutos %= 60;
     horas %= 60;
 
-    return (dias + "dias" + "horas" + minutos + "minutos" + segundos + "segundos");
-    
+    return (dias + " dias " + " horas " + minutos + " minutos " + segundos + " segundos ");
+
 }
